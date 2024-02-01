d178fc673a2827bad7a2f7e529af5405b5d5b2ea
Axilotl <axilotl17@gmail.com>
2024-01-31 19:15:12 -0800

    treble added

--- staff.js
+++ staff.js
@@ -2,24 +2,27 @@ const staff = document.getElementById("staff")
 const ctx = staff.getContext("2d");
 ctx.fillStyle = "green";
 
-staff.height = 400
+staff.height = 1600
+staff.width = (staff.height/staff.clientHeight)*staff.clientWidth
 
 window.addEventListener("resize", function(){
-    staff.width = staff.clientWidth
+    staff.width = (staff.height/staff.clientHeight)*staff.clientWidth
 })
 
 function staffDraw() {
+    ctx.lineWidth = 8;
     [5, 6, 7, 8, 9, 16, 17, 18, 19, 20].forEach(y => {
         ctx.beginPath();
-        ctx.moveTo(0, 16*y);
-        ctx.lineTo(400, 16*y);
+        ctx.moveTo(0, 64*y);
+        ctx.lineTo(staff.width, 64*y);
         ctx.stroke()
     });
+    img = new Image();
+    img.onload = function() {
+    ctx.drawImage(img,0,0,800,2187,20,210,180,480);
+    console.log("a")
+    };
+    img.src = 'resources/GClef.png';
 }
-staffDraw()
 
-drawing = new Image();
-drawing.src = "treble.png";
-drawing.onload = function() {
-   ctx.drawImage(drawing,0,0);
-};
+staffDraw()
