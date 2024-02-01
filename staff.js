3c3e8340a65b76fe68f891e4cff275d55a58f3ff
Axilotl <axilotl17@gmail.com>
2024-01-31 20:49:23 -0800

    bass clef

--- staff.js
+++ staff.js
@@ -1,28 +1,48 @@
-const staff = document.getElementById("staff")
-const ctx = staff.getContext("2d");
-ctx.fillStyle = "green";
+const staffCanv = document.getElementById("staff")
+const overlayCanv = document.getElementById("overlay")
 
-staff.height = 1600
-staff.width = (staff.height/staff.clientHeight)*staff.clientWidth
+const staff = staffCanv.getContext("2d");
+const overlay = overlayCanv.getContext("2d");
+staff.fillStyle = "green";
+
+staffCanv.height = 1600
+overlay.height = 1600
+staffCanv.width = (staffCanv.height/staffCanv.clientHeight)*staffCanv.clientWidth
 
 window.addEventListener("resize", function(){
-    staff.width = (staff.height/staff.clientHeight)*staff.clientWidth
+    staffCanv.width = (staffCanv.height/staffCanv.clientHeight)*staffCanv.clientWidth
+    staffDraw()
 })
 
 function staffDraw() {
-    ctx.lineWidth = 8;
+    staff.beginPath();
+    staff.lineWidth = 8;
     [5, 6, 7, 8, 9, 16, 17, 18, 19, 20].forEach(y => {
-        ctx.beginPath();
-        ctx.moveTo(0, 64*y);
-        ctx.lineTo(staff.width, 64*y);
-        ctx.stroke()
+        staff.moveTo(0, 64*y);
+        staff.lineTo(staffCanv.width, 64*y);
+        staff.stroke()
     });
-    img = new Image();
-    img.onload = function() {
-    ctx.drawImage(img,0,0,800,2187,20,210,180,480);
-    console.log("a")
+    treble = new Image();
+    treble.onload = function() {
+    staff.drawImage(treble,0,0,800,2187,20,210,180,480);
     };
-    img.src = 'resources/GClef.png';
+    treble.src = 'resources/GClef.png';
+    bass = new Image();
+    bass.onload = function() {
+    staff.drawImage(bass,0,0,1280,1422,20,1020,200,220);
+    };
+    bass.src = 'resources/FClef.png';
+    staff.closePath();
 }
+
+function drawOverlay(staff, n) {
+    overlay.beginPath();
+    overlay.fillRect(20, 20, 40, 40)
+    overlay.stroke()
+    overlay.fillStyle = "rgba(255, 255, 0, 0.5)";
+
+}
+
+
 
 staffDraw()
